5ef28b519316ab523ca15b3b8a5887ae7f2669ab
Tom Sasani <tomsasani1493@gmail.com>
2023-01-26 07:34:09 -0700

    use args instead of hard coded paths

--- scripts/manhattan.py
+++ scripts/manhattan.py
@@ -5,51 +5,65 @@ import numpy as np
 import statsmodels.stats.multitest as mt
 import glob
 import scipy.stats as ss
+import argparse
+from sklearn.linear_model import LinearRegression
 
-PROJDIR = "/scratch/ucgd/lustre-work/quinlan/u1006375/proj-bxd"
+def main(args):
 
-results = pd.read_csv(f"{PROJDIR}/o")
-markers = pd.read_csv(f"{PROJDIR}/data/genotypes/BXD.markers")
+    results = pd.read_csv(args.results)
+    markers = pd.read_csv(args.markers)
 
-results_merged = results.merge(markers, on="marker")
+    results_merged = results.merge(markers, on="marker")
 
-# try out method for 95% Bayes CI
-# rescale cosine distances to be between 0 and 1
-for chrom, chrom_df in results_merged.groupby('chromosome'):
-    min_dist, max_dist = min(chrom_df['distance']), max(chrom_df['distance'])
-    chrom_df['norm_distance'] = chrom_df['distance'].apply(lambda d: (d - min_dist) / (max_dist - min_dist))
-    if chrom == "14":
-        f, ax = plt.subplots()
-        #cumsum = np.cumsum(chrom_df['norm_distance'])
-        chrom_df['cumsum'] = chrom_df['distance'] / np.sum(chrom_df['distance'])
-        sns.displot(chrom_df, x="Mb", y="distance")
-        f.savefig('distplot.png')
+    # fit PCA on genotype matrix
+    X, y = results_merged["genetic_difference"].values, results_merged["distance"].values
+    reg = LinearRegression().fit(X.reshape(-1, 1), y)
+    preds = reg.predict(X.reshape(-1, 1))
+    resids = preds - y 
+    results_merged["residuals"] = resids * -1
 
-# # compute approx. genetic length of each chromosome
-# max_length_per_chrom = results_merged.groupby('chromosome').agg({'cM': max}).reset_index().rename(columns={'cM': 'genetic_length'})
-# chrom_lens = max_length_per_chrom['genetic_length']
-# max_length_per_chrom['frac'] = chrom_lens / np.sum(chrom_lens)
+    f, ax = plt.subplots()
+    ax.scatter(y, preds)
+    f.savefig('resids.png', dpi=300)
 
-# # compute genome-adjusted p-values 
-# results_merged = results_merged.merge(max_length_per_chrom, on="chromosome")
 
-# results_merged['adj_pval'] = results_merged.apply(lambda row: 1 - ((1 - row['pval']) ** row['frac']), axis=1)
-# #results_merged['-log10(p)'] = results_merged['adj_pval'].apply(lambda p: )
+    # # compute approx. genetic length of each chromosome
+    # max_length_per_chrom = results_merged.groupby('chromosome').agg({'cM': max}).reset_index().rename(columns={'cM': 'genetic_length'})
+    # chrom_lens = max_length_per_chrom['genetic_length']
+    # max_length_per_chrom['frac'] = chrom_lens / np.sum(chrom_lens)
 
-signif = results_merged[results_merged["distance"] >= results_merged["suggestive_percentile"]]
-signif.to_csv(f"{PROJDIR}/significant_markers.csv", index=False)
+    # # compute genome-adjusted p-values 
+    # results_merged = results_merged.merge(max_length_per_chrom, on="chromosome")
 
-g = sns.FacetGrid(results_merged, row="chromosome", sharex=False, aspect=2.5, sharey=True)
-g.map(sns.scatterplot,
-    "Mb",
-    "distance",
-    "k",
-    palette="colorblind",
-)
-axes = g.axes[0]
-for label, color in zip(('suggestive', 'significant'), ("dodgerblue", "firebrick")):
-    max_dist = results_merged[f'{label}_percentile'].unique()[0]
-    g.map(plt.axhline, y=max_dist, ls=":", c=color, label=f"Genome-wide {label} threshold")
-g.add_legend()
-g.tight_layout()
-g.savefig('manhattan.png', dpi=300)
+    # results_merged['adj_pval'] = results_merged.apply(lambda row: 1 - ((1 - row['pval']) ** row['frac']), axis=1)
+    # #results_merged['-log10(p)'] = results_merged['adj_pval'].apply(lambda p: )
+
+    results_merged['odds_ratio'] = results_merged['distance'] / results_merged['null_mean']
+    results_merged['suggestive_odds_ratio'] = results_merged['suggestive_percentile'] / results_merged['null_mean']
+    results_merged['significant_odds_ratio'] = results_merged['significant_percentile'] / results_merged['null_mean']
+
+    signif = results_merged[results_merged["distance"] >= results_merged["suggestive_percentile"]]
+    signif.to_csv(f"{args.outpref}.significant_markers.csv", index=False)
+
+    g = sns.FacetGrid(results_merged, row="chromosome", sharex=False, aspect=2.5, sharey=True)
+    g.map(sns.scatterplot,
+        "Mb",
+        "odds_ratio",
+        "k",
+        palette="colorblind",
+    )
+    axes = g.axes[0]
+    for label, color in zip(('suggestive', 'significant'), ("dodgerblue", "firebrick")):
+        max_dist = results_merged[f'{label}_odds_ratio'].unique()[0]
+        g.map(plt.axhline, y=max_dist, ls=":", c=color, label=f"Genome-wide {label} threshold")
+    g.add_legend()
+    g.tight_layout()
+    g.savefig(f"{args.outpref}.manhattan_plot.png", dpi=300)
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument("--results")
+    p.add_argument("--markers")
+    p.add_argument("--outpref", type=str, default=".")
+    args = p.parse_args()
+    main(args)
