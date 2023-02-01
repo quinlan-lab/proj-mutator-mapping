d73d3d11949ddf24c91067049bc0fd083d9335bc
Tom Sasani <tomsasani1493@gmail.com>
2023-02-01 08:11:49 -0700

    add basic pandera schema

---
+++ scripts/schema.py
@@ -0,0 +1,24 @@
+import pandera as pa
+from pandera import Column, Check, DataFrameSchema
+from typing import Optional
+
+
+IHDResultSchema = DataFrameSchema({
+    "marker": Column(str),
+    "distance": Column(float, Check.in_range(min_value=-1, max_value=1)),
+    "k": Column(int, Check.is_in([1, 3])),
+})
+
+
+MutationSchema = DataFrameSchema({
+    "sample": Column(str),
+    "kmer": Column(str, Check.str_matches(r"[ATCGN]{3}>[ATCGN]{3}")),
+    "count": Column(int, Check.greater_than_or_equal_to(1)),
+})
+
+
+MarkerMetadataSchema = DataFrameSchema({
+    "marker": Column(str),
+    "cM": Column(float, required=False),
+    "Mb": Column(float, required=False),
+})
