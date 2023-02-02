9b2750eecdc68a5d39f10fe8c74033724109eb41
Tom Sasani <tomsasani1493@gmail.com>
2023-02-01 20:43:41 -0500

    match schema names with true column names

--- scripts/schema.py
+++ scripts/schema.py
@@ -5,17 +5,15 @@ from typing import Optional
 
 IHDResultSchema = DataFrameSchema({
     "marker": Column(str),
-    "distance": Column(float),#, Check.in_range(min_value=-1, max_value=2)),
+    "Distance": Column(float, Check.in_range(min_value=-1, max_value=2)),
     "k": Column(int, Check.isin([1, 3])),
 })
-
 
 MutationSchema = DataFrameSchema({
     "sample": Column(str),
     "kmer": Column(str, Check.str_matches(r"[ATCGN]{3}>[ATCGN]{3}")),
     "count": Column(int, Check.greater_than_or_equal_to(1)),
 })
-
 
 MarkerMetadataSchema = DataFrameSchema({
     "marker": Column(str),
