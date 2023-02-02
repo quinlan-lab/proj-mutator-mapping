02164a2ab0363df37658ef9c4a01d92b1bb6ee5d
Tom Sasani <tomsasani1493@gmail.com>
2023-02-01 20:20:55 -0500

    per-chromosome plots

--- scripts/plot_ihd_results.py
+++ scripts/plot_ihd_results.py
@@ -7,8 +7,7 @@ from schema import IHDResultSchema, MarkerMetadataSchema
 
 def main(args):
 
-    # read in results of IHD scan and validate with
-    # pandera
+    # read in results of IHD scan and validate with pandera
     results = pd.read_csv(args.results)
     IHDResultSchema.validate(results)
 
@@ -18,11 +17,8 @@ def main(args):
 
     results_merged = results.merge(markers, on="marker")
 
-    signif_thresh = -1 * np.log10(0.05 / results_merged.shape[0])
-
     # get significant markers
-    signif = results_merged[results_merged["distance"] >= results_merged["suggestive_percentile"]]
-
+    signif = results_merged.query("distance >= suggestive_percentile")
     signif.to_csv(f"{args.outpref}.significant_markers.csv", index=False)
 
     # plot manhattan
@@ -30,6 +26,7 @@ def main(args):
     g.map(sns.scatterplot,
         args.colname,
         "distance",
+        color="lightgrey",
     )
     for label, level, color in zip(
         ('Suggestive distance threshold', 'Significant distance threshold'),
@@ -38,10 +35,23 @@ def main(args):
     ):
         max_dist = results_merged[f'{level}_percentile'].unique()[0]
         g.map(plt.axhline, y=max_dist, ls=":", c=color, label=label)
-        
+
     g.add_legend()
     g.tight_layout()
     g.savefig(f"{args.outpref}.manhattan_plot.png", dpi=300)
+
+    if args.chrom is not None:
+        results_merged_chr = results_merged[results_merged["chromosome"] == args.chrom]
+        f, ax = plt.subplots()
+        sns.scatterplot(
+            results_merged_chr,
+            x=args.colname,
+            y="Distance",
+            color="lightgrey",
+            ax=ax,
+        )
+        f.savefig(f"{args.outpref}.{args.chrom}.manhattan_plot.png", dpi=300)
+
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
@@ -67,6 +77,11 @@ if __name__ == "__main__":
         default="Mb",
         help=
         "Name of the column in `--markers` that denotes the position you which to plot on the x-axis of the Manhattan plot. Default is 'Mb'",
+    )
+    p.add_argument(
+        "-chrom",
+        default=None,
+        help="Chromosome to display separately in its own Manhattan plot.",
     )
     args = p.parse_args()
     main(args)
