d4ba2bc3fc2a27b9fe093edaf14a2831bcf1fe7c
Tom Sasani <tomsasani1493@gmail.com>
2023-03-28 12:44:04 -0400

    add Rscript for QTL mapping using simulated data

---
+++ scripts/map_qtl.R
@@ -0,0 +1,76 @@
+library(qtl2)
+library(optparse)
+
+option_list <- list(
+  make_option(c("-j", "--json"), type="character", default=NULL),
+  make_option(c("-p", "--phenotype_file"), type="character", default=NULL),
+  make_option(c("-o", "--output_file"), type="character", default=NULL))
+
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+
+# read in the JSON that directs R/qtl2 to sample genotypes,
+# phenotypes, and covariates
+sim_bxd <- read_cross2(opt$json)
+
+# read in the phenotype values for each BXD strain
+phen_df <- read.csv(opt$phenotype_file, header=T)
+
+# calculate QTL genotype probabilities
+pr <- calc_genoprob(sim_bxd, sim_bxd$pmap, error_prob = 0.002, map_function = "c-f")
+
+# calculate kinship between strains using the
+# "overall" method
+k <- calc_kinship(pr, "overall")
+
+# store results of simulated QTL scans
+res_df <- data.frame(trial = 0,
+                     Power = 0)
+
+trials <- 100
+
+for (trial_n in 1:trials) {
+    
+    # subset the phenotype data for this trial and convert to matrix
+    phenotype <- c("C_A")
+    phen_df_sub = subset(phen_df, trial == trial_n - 1)
+    phen_matrix <- as.matrix(phen_df_sub[phenotype])
+
+
+    strain_names <- phen_df_sub$sample
+    rownames(phen_matrix) <- strain_names
+
+    # perform a genome scan, accounting for kinship and
+    # epoch as an additive covarirate
+    out <- scan1(pr, phen_matrix)
+
+    # perform a permutation test to assess significance
+    operm <- scan1perm(pr, phen_matrix, n_perm=1000)
+
+    # get the LOD threshold for a < 0.05
+    lod_cutoff = summary(operm, alpha=0.05 / 7)[1]
+
+    # print LOD peaks
+    peaks = find_peaks(out, sim_bxd$pmap, threshold=lod_cutoff)
+    n_peaks = dim(peaks)[1]
+    discovered = 0
+    if (n_peaks > 0) {
+        # marker at which a QTL was found
+        predicted_marker = peaks[,4]
+        focal_marker_idx = phen_df_sub$focal_marker[1] + 1
+        true_marker = sim_bxd$pmap$`1`[focal_marker_idx]
+        if (predicted_marker == true_marker) {
+            discovered = 1
+        }
+
+    }
+
+    # append results
+    hsq_df <- data.frame(trial = trial_n,
+                    Power = discovered)
+
+    res_df <- rbind(res_df, hsq_df)
+}
+
+
+write.csv(res_df, opt$output_file)
