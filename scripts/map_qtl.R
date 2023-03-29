544a7b902cfb74d49d37c6ad0be88d34e49734f0
Tom Sasani <tomsasani1493@gmail.com>
2023-03-29 09:51:24 -0400

    use uncorrected or corrected alpha values in qtl mapping

--- scripts/map_qtl.R
+++ scripts/map_qtl.R
@@ -4,7 +4,8 @@ library(optparse)
 option_list <- list(
   make_option(c("-j", "--json"), type="character", default=NULL),
   make_option(c("-p", "--phenotype_file"), type="character", default=NULL),
-  make_option(c("-o", "--output_file"), type="character", default=NULL))
+  make_option(c("-o", "--output_file"), type="character", default=NULL),
+  make_option(c("-m", "--mutation_type", type="character", default=NULL)))
 
 opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
@@ -25,6 +26,7 @@ k <- calc_kinship(pr, "overall")
 
 # store results of simulated QTL scans
 res_df <- data.frame(trial = 0,
+                     bonferroni_corr = 1,
                      Power = 0)
 
 trials <- 100
@@ -32,7 +34,7 @@ trials <- 100
 for (trial_n in 1:trials) {
     
     # subset the phenotype data for this trial and convert to matrix
-    phenotype <- c("C_A")
+    phenotype <- c(opt$mutation_type)
     phen_df_sub = subset(phen_df, trial == trial_n - 1)
     phen_matrix <- as.matrix(phen_df_sub[phenotype])
 
@@ -47,29 +49,34 @@ for (trial_n in 1:trials) {
     # perform a permutation test to assess significance
     operm <- scan1perm(pr, phen_matrix, n_perm=1000)
 
-    # get the LOD threshold for a < 0.05
-    lod_cutoff = summary(operm, alpha=0.05 / 7)[1]
+    for (bc in c(1, 7)) {
 
-    # print LOD peaks
-    peaks = find_peaks(out, sim_bxd$pmap, threshold=lod_cutoff)
-    n_peaks = dim(peaks)[1]
-    discovered = 0
-    if (n_peaks > 0) {
-        # marker at which a QTL was found
-        predicted_marker = peaks[,4]
-        focal_marker_idx = phen_df_sub$focal_marker[1] + 1
-        true_marker = sim_bxd$pmap$`1`[focal_marker_idx]
-        if (predicted_marker == true_marker) {
-            discovered = 1
+        # get the LOD threshold for a < 0.05
+        lod_cutoff = summary(operm, alpha=0.05 / bc)[1]
+
+        # print LOD peaks
+        peaks = find_peaks(out, sim_bxd$pmap, threshold=lod_cutoff)
+        n_peaks = dim(peaks)[1]
+        discovered = 0
+        if (n_peaks > 0) {
+            # markers at which a QTL was found
+            predicted_markers = as.vector(peaks[,4])
+            focal_marker_idx = phen_df_sub$focal_marker[1] + 1
+            true_marker = sim_bxd$pmap$`1`[focal_marker_idx]
+
+            if (true_marker %in% predicted_markers) {
+                discovered = 1
+            }
+
         }
 
+        # append results
+        hsq_df <- data.frame(trial = trial_n,
+                        bonferroni_corr = bc,
+                        Power = discovered)
+
+        res_df <- rbind(res_df, hsq_df)
     }
-
-    # append results
-    hsq_df <- data.frame(trial = trial_n,
-                    Power = discovered)
-
-    res_df <- rbind(res_df, hsq_df)
 }
 
 
