7953a8c6fbf5b78486ae85fe976e008dce05fd33
Tom Sasani <tomsasani1493@gmail.com>
2023-03-30 16:41:51 -0400

    improve style of power plots

--- ihd/plot_power.py
+++ ihd/plot_power.py
@@ -8,7 +8,10 @@ plt.rc("font", size=12)
 def main(args):
     df = pd.read_csv(args.results)
     df['Power'] = df['pval'].apply(lambda p: p <= 0.05)
-    df["mutation_type"] = df["mutation_type"].apply(lambda m: m.replace(">", r"$\to$"))
+    df = df[df["mutation_type"].isin(["C_G", "C_A", "C_T"])]
+
+    df["mutation_type"] = df["mutation_type"].apply(lambda m: m.replace("_", r"$\to$"))
+    df["effect_size"] = df["effect_size"] / 100.
 
     replace_dict = {
         "mutation_type": "Mutation type",
@@ -20,9 +23,8 @@ def main(args):
     }
     df.rename(columns=replace_dict, inplace=True)
 
-
     g = sns.FacetGrid(data=df, row="Mutation type", col="# haplotypes", aspect=1.5)
-    g.map(sns.lineplot, "Mutator effect size", "Power", "# mutations")
+    g.map(sns.lineplot, "Mutator effect size", "Power", "# mutations", palette="colorblind")
     g.add_legend(title = "# of mutations\nper haplotype")
     g.tight_layout()
 
--- scripts/compare_ihd_qtl_power.py
+++ scripts/compare_ihd_qtl_power.py
@@ -14,13 +14,12 @@ def main(args):
     ihd_power['Power'] = ihd_power['pval'].apply(lambda p: p <= 0.05)
     ihd_power["Method"] = "IHD"
 
-    correction = 1
-
+    correction = 7
     ihd_power["bonferroni_corr"] = correction
-    combined_power = pd.concat([qtl_power, ihd_power]).reset_index()
 
-    # subset to a single mutation type 
-    combined_power = combined_power[combined_power["mutation_type"] == args.mutation_type]
+    combined_power = pd.concat([qtl_power, ihd_power]).reset_index()
+    combined_power = combined_power[combined_power["mutation_type"].isin(["C_A", "C_T"])]
+
     combined_power = combined_power[combined_power["bonferroni_corr"] == correction]
 
     combined_power["mutation_type"] = combined_power["mutation_type"].apply(lambda m: m.replace("_", r"$\to$"))
@@ -36,9 +35,11 @@ def main(args):
     }
     combined_power.rename(columns=replace_dict, inplace=True)
 
-    g = sns.FacetGrid(data=combined_power, row="# mutations", col="# haplotypes", aspect=1.5)
-    g.map(sns.lineplot, "Mutator effect size", "Power", "Method")
+    g = sns.FacetGrid(data=combined_power, row="# mutations", col="Mutation type", aspect=1.5)
+    g.map(sns.lineplot, "Mutator effect size", "Power", "Method", palette="colorblind")
     g.add_legend(title = "Method")
+    #g.fig.subplots_adjust(top=0.9)
+    #g.fig.suptitle('TITLE!')
     g.tight_layout()
 
     g.savefig(args.out, dpi=300)
@@ -48,6 +49,5 @@ if __name__ == "__main__":
     p.add_argument("--qtl_power", help="""Results of power simulations using QTL mapping""")
     p.add_argument("--ihd_power", help="""Results of power simulations using IHD""")
     p.add_argument("--out", help="""Name of output plot""")
-    p.add_argument("-mutation_type", default="C_A", help="""Name of mutation type for which to plot power.""")
     args = p.parse_args()
     main(args)
