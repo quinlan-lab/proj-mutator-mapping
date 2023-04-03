63412c3ee8b3eca39f4b38f3ff3147d6753ca9b2
Tom Sasani <tomsasani1493@gmail.com>
2023-04-03 12:18:34 -0400

    fix paths in epistasis testing script

--- scripts/test_for_epistasis.R
+++ scripts/test_for_epistasis.R
@@ -1,10 +1,9 @@
 library(MASS)
 library(ggplot2)
 
-df <- read.csv("/Users/tomsasani/quinlanlab/proj-mutator-mapping/csv/true_tidy_spectra.csv")
+df <- read.csv("/Users/tomsasani/quinlanlab/proj-mutator-mapping/csv/bxd_spectra_1mer.csv")
 
 df <- subset(df, is_ca == 1)
-
 library(qtl2)
 library(dplyr)
 library(coxme)
@@ -46,26 +45,28 @@ rownames(kinship) <- phen_df$sample
 
 gfit1 <- lm(Fraction ~ Epoch + Haplotype_A * Haplotype_B, data = phen_df)
 gfit1 <- lmekin(Fraction ~ Epoch + Haplotype_A + Haplotype_B + (1 | sample),
-                 data = phen_df,
-                 varlist = kinship)
+    data = phen_df,
+    varlist = kinship
+)
 gfit2 <- lmekin(Fraction ~ Haplotype_A * Haplotype_B + (1 | sample),
-                 data = phen_df,
-                 varlist = kinship)
+    data = phen_df,
+    varlist = kinship
+)
 
 print(gfit2)
-#print (anova(gfit2, gfit1))
-
 m1 <- glm(Count ~ offset(log(ADJ_AGE)) + Haplotype_A + Haplotype_B,
-            data = df,
-            family = poisson(link = "log"))
+    data = df,
+    family = poisson(link = "log")
+)
 m2 <- glm(Count ~ offset(log(ADJ_AGE)) + Haplotype_A * Haplotype_B,
-            data = df,
-            family = poisson(link = "log"))
-print (anova(m2, m1, test = "Chisq"))
+    data = df,
+    family = poisson(link = "log")
+)
+print(anova(m2, m1, test = "Chisq"))
 
-df = read.csv("/Users/tomsasani/quinlanlab/proj-mutator-mapping/csv/dumont_tidy.csv")
+df <- read.csv("/Users/tomsasani/quinlanlab/proj-mutator-mapping/csv/mgp_spectra.csv")
 
-df = subset(df, is_ca == 1)
-m1 <- glm(Count ~ offset(log(CALLABLE_C)) + Haplotype_A + Haplotype_B, data = df, family=poisson(link="log"))
-m2 <- glm(Count ~ offset(log(CALLABLE_C)) + Haplotype_A * Haplotype_B, data = df, family=poisson(link="log"))
-print (anova(m2, m1, test="Chisq"))
+df <- subset(df, is_ca == 1)
+m1 <- glm(Count ~ offset(log(CALLABLE_C)) + Haplotype_A + Haplotype_B, data = df, family = poisson(link = "log"))
+m2 <- glm(Count ~ offset(log(CALLABLE_C)) + Haplotype_A * Haplotype_B, data = df, family = poisson(link = "log"))
+print(anova(m2, m1, test = "Chisq"))
