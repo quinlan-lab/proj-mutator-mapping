7d41b37a3b808a3c5fa0eda7a0c1d90b47115b38
Tom Sasani <tomsasani1493@gmail.com>
2023-04-03 12:18:24 -0400

    use stackedbar for sbs activity

--- scripts/plot_signature_activity.py
+++ scripts/plot_signature_activity.py
@@ -14,17 +14,19 @@ def main(args):
                 ["#398D84", "#E67F3A", "#EBBC2C", "#2F294A"],
             ))
     
-    sig_dict = {"SBS1": r"$\bf{SBS1}$" + "\n(Spontaneous deamination of 5-mC)",
-                "SBS5": r"$\bf{SBS5}$" + "\n(Unknown clock-like)",
-                "SBS18": r"$\bf{SBS18}$" + "\n(Damage by ROS)",
-                "SBS30": r"$\bf{SBS30}$" + "\n(Defective BER due to NTHL1)"}
+    sig_dict = {"SBS1": r"$\bf{SBS1}$" + " (Spontaneous deamination of 5-mC)",
+                "SBS5": r"$\bf{SBS5}$" + " (Unknown clock-like)",
+                "SBS18": r"$\bf{SBS18}$" + " (Damage by ROS)",
+                "SBS30": r"$\bf{SBS30}$" + " (Defective BER due to NTHL1)"}
+
+    color_dict = dict(zip(sig_dict.values(), ["gainsboro", "dimgrey", "#E67F3A", "darkgrey"]))
 
     mutations = pd.read_csv(args.spectra)[["sample", "Haplotypes", "Generations"]]
     activities = pd.read_csv(args.activities, sep="\t")
 
     # convert activities to a tidy dataframe
     activities_tidy = activities.melt(id_vars="Samples", var_name="Signature", value_name="Count")
-    activities_tidy = activities_tidy#.query('Signature == "SBS18"')
+    activities_tidy = activities_tidy
     activities_tidy["Signature"] = activities_tidy["Signature"].apply(lambda s: sig_dict[s])
     # figure out the total sum of mutations in each sample that was assigned
     # to a specific SBS signatures
@@ -34,33 +36,36 @@ def main(args):
     activities_tidy["Fraction"] = activities_tidy["Count"] / activities_tidy["Total"]
     # annotate each sample's SBS activities with relevant metadata
     annotated_activities = activities_tidy.merge(mutations, left_on="Samples", right_on="sample").sort_values("Haplotypes")
+
     f, ax = plt.subplots(figsize=(18, 6))
-    sns.barplot(
-        data=annotated_activities,
-        y="Signature",
-        x="Fraction",
-        hue="Haplotypes",
-        palette=palette,
-        capsize=0.05,
-        errcolor="k",
-        ec="k",
-        linewidth=2,
-        ax=ax,
-    )
+
+    per_sig_sums = annotated_activities.groupby(["Haplotypes", "Signature"]).agg({"Count": sum}).reset_index()
+    per_hap_totals = per_sig_sums.groupby("Haplotypes").agg({"Count": sum}).reset_index().rename(columns={"Count": "Total"})
+    per_sig_fracs = per_sig_sums.merge(per_hap_totals, on="Haplotypes")
+    per_sig_fracs["Fraction"] = per_sig_fracs["Count"] / per_sig_fracs["Total"]
+    per_sig_fracs_wide = per_sig_fracs.drop(columns=["Count", "Total"]).pivot(index="Haplotypes", columns="Signature")
+    new_cols = [c[1] for c in per_sig_fracs_wide.columns]
+    per_sig_fracs_wide.columns = new_cols
+    per_sig_fracs_wide.loc[["D-D", "D-B", "B-D", "B-B"]].plot.barh(color=color_dict, ax=ax, stacked=True, ec="k", lw=2,)
+    
+
     sns.despine(ax=ax, top=True, right=True)
     ax.set_xlabel("Fraction of mutations attributed to signature")
-    ax.set_ylabel(None)
+    ax.set_ylabel("Genotypes at chr4 and chr6 peaks")
+    ax.tick_params(axis='y', which='both',length=0, pad=10)
     handles, labels = ax.get_legend_handles_labels()
     l = plt.legend(
         handles,
         labels,
-        title="Genotypes at chr4 and chr6 peaks",
         frameon=False,
+        title="COSMIC SBS signature",
+        bbox_to_anchor=(1, 0.75)
     )
 
     # change all spine widths
     for axis in ['top','bottom','left','right']:
-        ax.spines[axis].set_linewidth(1.5)
+        width = 1.5
+        ax.spines[axis].set_linewidth(width)
 
     # increase tick width
     ax.tick_params(width=1.5)
